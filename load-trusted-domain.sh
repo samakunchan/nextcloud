4206235b6415aa6fd2e5cc1df136dd2af4ff930d
samakunchan <cedric.badjah@gmail.com>
2025-10-02 02:17:57 +0200

    fix: https avec docker

---
+++ load-trusted-domain.sh
@@ -0,0 +1,3 @@
+# Possible que ça ne sert plus à rien, mais je laisse la parce que j'ai oublier pourquoi je l'avais mis.
+# Possible qu'il a été utile quand mes recherches de debug.
+sudo docker exec -u www-data -it nextcloud php occ config:system:set trusted_domains 1 --value=nextcloudsama.devpapangue.com
